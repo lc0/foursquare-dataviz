e3287e1f3955b78e5de19b354786843bbc67bc39
Sergii Khomenko <sergii.khomenko@stylight.com>
2014-12-27 00:19:01 +0100

    we do not actually know the number of checkins limited by timeframe

--- global.R
+++ global.R
@@ -15,37 +15,29 @@ import.packages <- function(pkg) {
 foursquare.getCheckins <- function(token, v, startStr, endStr){
   startTS <- as.numeric(as.POSIXct(startStr, format="%Y-%m-%d %H:%M:%S"))
   endTS <- as.numeric(as.POSIXct(endStr, format="%Y-%m-%d %H:%M:%S"))
-  
+
   limit <- 250
   offset <- 0
+
+  name = vector("character")
+  country = vector("character")
+  city = vector("character")
+  lat = vector("double")
+  lng = vector("double")
+  likes = vector("integer")
+  category = vector("character")
+  created = vector("character")
   
-  urlTemplate <- "https://api.foursquare.com/v2/users/self/checkins?oauth_token=%s&v=%s&afterTimestamp=%s&beforeTimestamp=%s&limit=%i&offset=%i"
-  apiUrl <- sprintf(urlTemplate, token, v, startTS, endTS, limit, offset)
-  json<-getURL(apiUrl, cainfo="cacert.pem")
-  objects<-fromJSON(json, encoding="UTF-8")
-  
-  if (is.null(objects$response$checkins$count)) {
-    print("Can't get api result, check your credentials")
-    return()
-  }
-  
-  count <- objects$response$checkins$count
-  print(sprintf("Found %s events", count))
-  
-  name = vector("character", length=count)
-  country = vector("character", length=count)
-  city = vector("character", length=count)
-  lat = vector("double", length=count)
-  lng = vector("double", length=count)
-  likes = vector("integer", length=count)
-  category = vector("character", length=count)
-  created = vector("character", length=count)
-  
-  while (offset < count) {
+  while (TRUE) {
     urlTemplate <- "https://api.foursquare.com/v2/users/self/checkins?oauth_token=%s&v=%s&afterTimestamp=%s&beforeTimestamp=%s&limit=%i&offset=%i"
     apiUrl <- sprintf(urlTemplate, token, v, startTS, endTS, limit, offset)
     json<-getURL(apiUrl, cainfo="cacert.pem", .mapUnicode=TRUE)
     objects<-fromJSON(json)
+    
+    if (is.null(objects$response$checkins$count)) {
+      print("Can't get api result, check your credentials")
+      return()
+    }
     
     checkins <- objects$response$checkins$items
     
@@ -79,6 +71,8 @@ foursquare.getCheckins <- function(token, v, startStr, endStr){
   
   # Encoding(foursquareData$country) <- "utf8"
   # Encoding(foursquareData$city) <- "utf8"
+  
+  print(sprintf("Fetched %s checkins", length(country)))
   
   checkins.df<-as.data.frame(list(country=country, city=city, lat=lat, lng=lng, category=category, created=created, name=name), stringsAsFactors=FALSE)
   return(checkins.df)
