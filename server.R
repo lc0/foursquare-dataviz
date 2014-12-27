2ae64d231a1488f09d7fd20c4ab5b4505e58d36b
Sergii Khomenko <sergii.khomenko@stylight.com>
2014-12-27 02:14:38 +0100

    tricky issues with old factor levels

--- server.R
+++ server.R
@@ -1,8 +1,9 @@
 library(leaflet)
 library(ggplot2)
+library(ShinyDash)
 
 shinyServer(function(input, output, session) {
-  
+
   map <- createLeafletMap(session, "map")
 
   #Encoding(foursquareData2013$country) <- "no"
@@ -11,36 +12,36 @@ shinyServer(function(input, output, session) {
 
   # Encoding(foursquareData[["2014"]]$city) <- "unknown"
   # foursquareData$city <- enc2native(foursquareData$city)
-  
+
   # print(foursquareData[["2014"]]$country)
   # foursquareData[["2014"]]$country <- shiny::HTML(foursquareData[["2014"]]$country)
   # print(foursquareData[["2014"]]$country)
-  
+
   session$onFlushed(once=TRUE, function() {
-    #map$addMarker(foursquareData$lat[foursquareData$year == 2014], 
+    #map$addMarker(foursquareData$lat[foursquareData$year == 2014],
     #              foursquareData$lng[foursquareData$year == 2014])
   })
-  
-  
+
+
   values <- reactiveValues(selectedFeature = NULL)
-  
+
   observe({
     evt <- input$map_click
     if (is.null(evt))
       return()
-    
+
     isolate({
       # An empty part of the map was clicked.
       # Null out the selected feature.
       values$selectedFeature <- NULL
     })
   })
-  
+
   observe({
     evt <- input$map_geojson_click
     if (is.null(evt))
       return()
-    
+
     isolate({
       # A GeoJSON feature was clicked. Save its properties
       # to selectedFeature.
@@ -62,48 +63,50 @@ shinyServer(function(input, output, session) {
 
   # The cities that are within the visible bounds of the map
   checkinsInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(uspop2000[FALSE,])
+
     bounds <- input$map_bounds
     latRng <- range(bounds$north, bounds$south)
     lngRng <- range(bounds$east, bounds$west)
 
-    subset(foursquareData,
+    newdf <- subset(foursquareData,
            lat >= latRng[1] & lat <= latRng[2] &
-             lng >= lngRng[1] & lng <= lngRng[2] & year == checkinYear() )
+             lng >= lngRng[1] & lng <= lngRng[2] & year==checkinYear()
+    )
+    newdf <-droplevels(newdf)
   })
+
 
 
   checkinYear <- reactive({
     map$clearShapes()
     map$clearMarkers()
-    map$addMarker(foursquareData$lat[foursquareData$year == input$year], 
+    map$addMarker(foursquareData$lat[foursquareData$year == input$year],
                   foursquareData$lng[foursquareData$year == input$year]
                   #, list(title="foursquareData[[input$year]]$name")
                   )
-    map$addCircle(foursquareData$lat[foursquareData$year == input$year], 
-                  foursquareData$lng[foursquareData$year == input$year])
-    
-    # TODO: here we can have some caching and loading new years
+    #map$addCircle(foursquareData$lat[foursquareData$year == input$year],
+    #              foursquareData$lng[foursquareData$year == input$year])
+
     input$year
   })
 
   output$citiesPlot <- renderPlot({
     cities <- table(checkinsInBounds()$city)
+
     names_c <- names(cities)
-    Encoding(names_c) <- 'utf8'
+    #Encoding(names_c) <- 'utf8'
 
     cities_df <- data.frame(cities = names_c, checkins = as.vector(cities))
-    
-    p <- ggplot(cities_df, aes(x=reorder(cities, - checkins), y=log(checkins))) + 
+
+    p <- ggplot(cities_df, aes(x=reorder(cities, - checkins), y=log(checkins))) +
       stat_summary(fun.y = sum, geom = "bar") +
       theme(axis.text.x=element_text(angle=90)) +
       geom_text(label=cities_df$checkins, vjust = -0.5)
-    
+
     #p <- barplot(cities)
-    
+
     print(p)
-    
+
   })
   output$countriesPlot <- renderPlot({
     if (length(checkinsInBounds()) == 0) {
@@ -112,25 +115,24 @@ shinyServer(function(input, output, session) {
     checkins <- table(checkinsInBounds()$country)
 
     names_c <- names(checkins)
-    Encoding(names_c) <- "utf8"
-    
+    #Encoding(names_c) <- "utf8"
+
     cities_df <- data.frame(cities = names_c, checkins = as.vector(checkins))
-    
-    p <- ggplot(cities_df, aes(x=reorder(cities, - checkins), y=log(checkins))) + 
+
+    p <- ggplot(cities_df, aes(x=reorder(cities, - checkins), y=log(checkins))) +
       stat_summary(fun.y = sum, geom = "bar") +
-      theme(axis.text.x=element_text(angle=90)) + 
+      theme(axis.text.x=element_text(angle=90)) +
       geom_text(label=cities_df$checkins, vjust = -0.5)
-    
+
     #p <- barplot(cities)
-    
+
     print(p)
-    
   })
-  
+
   output$countriesPlotLabel <- renderText({
     sprintf("Visited %i countries", length(table(checkinsInBounds()$country)))
   })
-  
+
   output$citiesPlotLabel <- renderText({
     sprintf("Visited %i cities", length(table(checkinsInBounds()$city)))
   })
@@ -140,7 +142,7 @@ shinyServer(function(input, output, session) {
     if (is.null(event))
       return()
     map$clearPopups()
-    
+
     #isolate({
     #  content <- as.character(tagList(
     #    tags$strong(paste(event)),
@@ -149,7 +151,7 @@ shinyServer(function(input, output, session) {
     #  map$showPopup(event$lat, event$lng, content, event$id)
     #})
   })
-  
+
   output$desc <- reactive({
     if (is.null(input$map_bounds))
       return(list())
